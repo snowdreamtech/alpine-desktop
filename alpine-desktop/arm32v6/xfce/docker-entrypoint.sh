16e51f525f4aee4a8f3da23f6d48421432ae7edb
snowdream <sn0wdr1am@icloud.com>
2019-11-27 01:01:44 +0800

    Update: docker-entrypoint.sh

--- alpine-desktop/arm32v6/xfce/docker-entrypoint.sh
+++ alpine-desktop/arm32v6/xfce/docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -e
 
-touch /root/.Xauthority
-mkdir /root/.vnc
+if [ ! -f "/root/.Xauthority" ]; then
+    touch /root/.Xauthorit
+fi
+
+if [ ! -d "/root/.vnc" ]; then
+    mkdir /root/.vnc
+fi
+
 x11vnc -storepasswd $VNC_PASSWORD /root/.vnc/passwd
 
 nohup /usr/bin/Xvfb $DISPLAY -screen 0 $RESOLUTION -ac +extension GLX +render -noreset > /dev/null 2>&1 &
 nohup startxfce4 > /dev/null 2>&1 &
-nohup x11vnc  -listen 0.0.0.0 -rfbauth /root/.vnc/passwd -rfbport $VNC_PORT -display $DISPLAY -wait 10 -forever -shared
+nohup x11vnc  -listen 0.0.0.0 -rfbauth /root/.vnc/passwd -rfbport $VNC_PORT -display $DISPLAY -wait 20 -loop -forever -shared
