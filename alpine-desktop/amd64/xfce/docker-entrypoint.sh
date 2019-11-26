ef3570b94a0e0eb1e3d2895d088c15743d7847d5
snowdream <sn0wdr1am@icloud.com>
2019-11-26 21:10:13 +0800

    Update: docker-entrypoint.sh
    Signed-off-by: snowdream <sn0wdr1am@icloud.com>

--- alpine-desktop/amd64/xfce/docker-entrypoint.sh
+++ alpine-desktop/amd64/xfce/docker-entrypoint.sh
@@ -7,4 +7,4 @@ x11vnc -storepasswd $VNC_PASSWORD /root/.vnc/passwd
 
 nohup /usr/bin/Xvfb $DISPLAY -screen 0 $RESOLUTION -ac +extension GLX +render -noreset > /dev/null 2>&1 &
 nohup startxfce4 > /dev/null 2>&1 &
-nohup x11vnc  -listen 0.0.0.0 -rfbauth /root/.vnc/passwd -rfbport $VNC_PORT -display $DISPLAY -wait 5 -forever -shared
+nohup x11vnc  -listen 0.0.0.0 -rfbauth /root/.vnc/passwd -rfbport $VNC_PORT -display $DISPLAY -wait 10 -forever -shared
