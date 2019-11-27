fb7463eb8700c2335b38d6e8f728dcb49046916d
snowdream <sn0wdr1am@icloud.com>
2019-11-27 19:26:59 +0800

    Add: manifest file

--- manifest-tool.sh
+++ manifest-tool.sh
@@ -2,5 +2,8 @@
 # https://github.com/estesp/manifest-tool
 
 # alpine-xfce
-manifest-tool push from-spec alpine-xfce.yaml
-manifest-tool push from-spec alpine-xfce-latest.yaml
+# manifest-tool push from-spec alpine-xfce.yaml
+# manifest-tool push from-spec alpine-xfce-latest.yaml
+
+manifest-tool push from-spec alpine-desktop.yaml
+manifest-tool push from-spec alpine-desktop-latest.yaml
